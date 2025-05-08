760271634a9a9897f0866b6524dd12c200f17432
anastasiya1512 <turukina.anastasiya@gmail.com>
2025-05-08 13:22:27 +0300

    Add files via upload

---
+++ lesson_01:training .py
@@ -0,0 +1,24 @@
+my_high=168
+print(my_high)
+
+my_name="Anastasiya"
+my_name="Anastasiya Pronina"
+print(my_name)
+
+pet_name=input("Tosha")
+print("Ваш любимчик -" + pet_name)
+
+def print_python():
+    print("Учу Python!")
+print_python()
+
+def print_letter(let):
+    print(let, end="")
+
+print_letter("С")
+print_letter("Т")
+print_letter("У")
+print_letter("Д")
+print_letter("Е")
+print_letter("Н")
+print_letter("Т")
