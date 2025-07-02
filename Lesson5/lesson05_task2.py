cf64ee26858fed7d645ab2452eae408d01271511
anastasiya1512 <turukina.anastasiya@gmail.com>
2025-07-02 13:08:15 +0300

    Add lesson 5 tasks

--- Lesson5/lesson05_task2.py
+++ Lesson5/lesson05_task2.py
@@ -3,10 +3,10 @@ from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome()
-driver.get("http//uitestingplayground.com/classattr")
+driver.get("http://uitestingplayground.com/dynamicid")
 time.sleep(2)
 
-blue_button = driver.find_element(By.CSS_SELECTOR, "btn-primary")
+blue_button = driver.find_element(By.CSS_SELECTOR, ".btn-primary")
 
 blue_button.click()
 time.sleep(2)
